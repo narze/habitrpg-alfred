9a218bd4f71cf48e25703480f742a4ee83f96e84
Manassarn Manoonchai <manassarn@gmail.com>
2014-02-15 01:02:33 +0700

    List tasks

--- workflow/hb.rb
+++ workflow/hb.rb
@@ -11,6 +11,61 @@ require 'net/http'
 require 'net/https'
 require 'json'
 
+def add_user_status_feedback(fb)
+  file = File.new("user.txt", "r")
+  api_user = file.gets.chomp
+  api_key = file.gets.chomp
+
+  uri = URI.parse("https://habitrpg.com:443/api/v2/user")
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+  req = Net::HTTP::Get.new(uri.path)
+  req['x-api-user'] = api_user
+  req['x-api-key'] = api_key
+  res = https.request(req)
+  user = JSON.parse(res.body)
+  hp = user["stats"]["hp"]
+  exp = user["stats"]["exp"]
+
+  # add an arbitrary feedback
+  fb.add_item({
+    :uid      => ""                     ,
+    :title    => "HP : #{hp.round} EXP : #{exp.round}"         ,
+    :subtitle => "HabitRPG"        ,
+    :arg      => "HP : #{hp.round} EXP : #{exp.round}" ,
+    :valid    => "yes"                  ,
+  })
+
+  fb
+end
+
+def add_tasks_feedback(fb)
+
+  file = File.new("user.txt", "r")
+  api_user = file.gets.chomp
+  api_key = file.gets.chomp
+
+  uri = URI.parse("https://habitrpg.com:443/api/v2/user/tasks")
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+  req = Net::HTTP::Get.new(uri.path)
+  req['x-api-user'] = api_user
+  req['x-api-key'] = api_key
+  res = https.request(req)
+  tasks = JSON.parse(res.body)
+
+  tasks.each do |task|
+    fb.add_item({
+      :uid      => ""                     ,
+      :title    => "[#{task["type"]}] #{task["text"]}"         ,
+      :subtitle => ""        ,
+      :arg      => "" ,
+      :valid    => "yes"                  ,
+    })
+  end
+
+  fb
+end
 Alfred.with_friendly_error do |alfred|
 
   alfred.with_rescue_feedback = true
@@ -38,40 +93,11 @@ Alfred.with_friendly_error do |alfred|
   else
     fb = alfred.feedback
 
-    # ... generate_feedback as usually
-
-    fb = alfred.feedback
-
-    file = File.new("user.txt", "r")
-    api_user = file.gets.chomp
-    api_key = file.gets.chomp
-
-    uri = URI.parse("https://habitrpg.com:443/api/v2/user")
-    https = Net::HTTP.new(uri.host, uri.port)
-    https.use_ssl = true
-    req = Net::HTTP::Get.new(uri.path)
-    req['x-api-user'] = api_user
-    req['x-api-key'] = api_key
-    res = https.request(req)
-    user = JSON.parse(res.body)
-    hp = user["stats"]["hp"]
-    exp = user["stats"]["exp"]
-
-    # add an arbitrary feedback
-    fb.add_item({
-      :uid      => ""                     ,
-      :title    => "HP : #{hp.round} EXP : #{exp.round}"         ,
-      :subtitle => "HabitRPG"        ,
-      :arg      => "HP : #{hp.round} EXP : #{exp.round}" ,
-      :valid    => "yes"                  ,
-    })
-
+    fb = add_user_status_feedback(fb)
+    fb = add_tasks_feedback(fb)
     # cache feedback
     fb.put_cached_feedback
 
     puts fb.to_xml(ARGV)
   end
 end
-
-
-
