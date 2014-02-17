c984fd436d85da06dddc2336d1772179a2a64e91
Manassarn Manoonchai <manassarn@gmail.com>
2014-02-17 22:22:02 +0700

    Add up/down task fuctionality

--- workflow/hb.rb
+++ workflow/hb.rb
@@ -83,6 +83,50 @@ def add_tasks_feedback(fb, query)
   fb
 end
 
+def add_task_direction_feedback(direction, fb, query)
+
+  file = File.new("user.txt", "r")
+  api_user = file.gets.chomp
+  api_key = file.gets.chomp
+
+  uri = URI.parse("https://habitrpg.com:443/api/v2/user/tasks")
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+  req = Net::HTTP::Get.new(uri.path)
+  req['x-api-user'] = api_user
+  req['x-api-key'] = api_key
+  res = https.request(req)
+  tasks = JSON.parse(res.body)
+
+  tasks.each do |task|
+    fb.add_item({
+      :uid      => "#{task["text"]}"                     ,
+      :title    => "#{direction.capitalize} : [#{task["type"]}] #{task["text"]}"         ,
+      :subtitle => ""        ,
+      :arg      => "tasks/#{task["id"]}/#{direction}" ,
+      # :valid    => "yes"                  ,
+      :autocomplete => "[#{task["type"]}] #{task["text"]}"
+    })
+  end
+
+  # push matches to the top
+  matches = []
+
+  fb.items.reject! do |item|
+    if query
+      u = item.uid
+      if u[0...query.length].downcase == query.downcase
+        matches << item
+        true
+      end
+    end
+  end
+
+  fb.items = matches.concat fb.items
+
+  fb
+end
+
 def add_coming_soon_feedback(fb)
   fb.add_item({
     :title    => "Coming Soon",
@@ -130,9 +174,9 @@ Alfred.with_friendly_error do |alfred|
     when "remove"
       fb = add_coming_soon_feedback(fb)
     when "up"
-      fb = add_coming_soon_feedback(fb)
+      fb = add_task_direction_feedback("up", fb, query)
     when "down"
-      fb = add_coming_soon_feedback(fb)
+      fb = add_task_direction_feedback("down", fb, query)
     else
       fb = add_user_status_feedback(fb)
     end
